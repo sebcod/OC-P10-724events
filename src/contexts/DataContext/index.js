34ad351d0b6cfd9482c433a3db70b283f3dc2263
sebcod <sebastien.codus@live.fr>
2024-01-13 10:33:54 +0100

    Fix:DataContext

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -16,27 +16,50 @@ export const api = {
   },
 };
 
+/*
+  add last data
+  add data error "error on calling events"
+*/
+
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
+  const [last, setLast] = useState(null);
   const [data, setData] = useState(null);
+
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
     } catch (err) {
-      setError(err);
+      setError("error on calling events"); // add error "error on calling events"
     }
   }, []);
+
+  // add last data
+  const getLastData = () => {
+    try {
+      setLast(
+        data?.events.sort((evtA, evtB) =>
+          new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+        )[0]
+      );
+    } catch (err) {
+      setError("error on calling events");
+    }
+  };
+
   useEffect(() => {
-    if (data) return;
+    if (last) return;
     getData();
-  });
-  
+    getLastData();
+  }, [data]);
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        last, // add last
       }}
     >
       {children}
@@ -46,7 +69,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
